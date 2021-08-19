8f60cbc4a43555707525a63ed95518fb94198c1d
jaysingh17 <jay.bagwansingh@ifuturz.com>
2021-08-19 19:26:01 +0530

    boxes are the correct length corresponding

--- inc/getassetscalendar.php
+++ inc/getassetscalendar.php
@@ -36,11 +36,10 @@ else
 {
 	foreach($assetsResult['assets'] as $x_value) 
 	{
-		$starttime = localtimezone('Y-m-d h:i:s',$x_value['details']['start_time']);
-		$endtime = localtimezone('Y-m-d h:i:s',$x_value['details']['end_time']);
+		$starttime = localtimezone('Y-m-d H:i:s',$x_value['details']['start_time']);
+		$endtime = localtimezone('Y-m-d H:i:s',$x_value['details']['end_time']);
 
 		$data[] = array(
-		  'id'   => $row["id"],
 		  'title'   => "Not Available",
 		  'start'   => $starttime,
 		  'end'   => $endtime
